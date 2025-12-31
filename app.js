74a46832fa693baa49255209e770811af4ee9c06
Ri-Reyan <rireyan50@gmail.com>
2025-12-31 19:15:20 +0600

    cors updated

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://vokings-client-hry51wbl8-ri-reyans-projects.vercel.app",
     credentials: true,
   })
 );
